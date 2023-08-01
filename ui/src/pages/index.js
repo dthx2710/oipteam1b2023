2c7e86ce3df91eeb33e6a6b40cd8010541940dc7
dthx2710 <dthx2710@hotmail.com>
2023-08-01 17:26:11 +0100

    feat: update home cta link to background

--- ui/src/pages/index.js
+++ ui/src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/ideation-process/aligning-mental-models"
+            to="/docs/ideation-process/background"
           >
             Ideation Process 💡
           </Link>
